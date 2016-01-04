742e6d989565b921798a6bedbe5ea24e64d50a63
Vincent de Bruijn <dbruijn@axini.com>
2016-01-04 22:23:46 +0100

    forgot to commit shopscreen prefab button script add thing. Let menu screen load asynchronously

--- Assets/IntroScript.cs
+++ Assets/IntroScript.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class IntroScript : MonoBehaviour {
 
 	private float time;
+	private AsyncOperation async;
 
 	// Use this for initialization
-	void Start () {
-	
+	IEnumerator Start() {
+		async = Application.LoadLevelAsync("menu");
+		async.allowSceneActivation = false;
+		yield return async;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		time += Time.deltaTime;
 		if (Input.GetMouseButtonDown(0) || time > 3) {
-			Application.LoadLevel ("menu");
+			async.allowSceneActivation = true;
 		}
 	}
 }
